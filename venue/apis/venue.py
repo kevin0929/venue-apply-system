8ed6c957cd2bce23b38b739a863f7e0b523f2dcb
kevin0929 <tn781927@yahoo.com.tw>
2024-08-27 00:21:37 +0800

    [Feature] Add new feature of apply and delete.

--- venue/apis/venue.py
+++ venue/apis/venue.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, url_for, flash, render_template, jsonify
+from flask import Blueprint, request, redirect, url_for, flash, render_template, jsonify, make_response
 
 from manager.venue import *
 from manager.user import *
@@ -24,16 +24,50 @@ def index(vid):
     return render_template("index.html", data=data)
 
 
-@venue_api.route("/<vid>/reserve/<date>", methods=["GET", "POST"])
-def reserve(vid, date):
+@venue_api.route("/<vid>/apply/<datetime>", methods=["GET", "POST"])
+def apply(vid, datetime):
     venue_manager = VenueManager()
     user_manager = UserManager()
     app_manager = ApplicationManager()
 
     # get current user and venue to relate application
-    venue = venue_manager.get_venue_by_id(vid)
+    current_venue = venue_manager.get_venue_by_id(vid)
 
     userid = session.get("userid")
     current_user = user_manager.get_user_by_id(userid)
 
-    app_manager.add_application(current_user, venue, date, 1)
+    # check this venue whether has been reserved by other user
+    conflict_flag = app_manager.check_application_conflict(venue=current_venue, datetime=datetime)
+    if conflict_flag:
+        return make_response('This venue has already reserved by other user', 409)
+
+    app_manager.add_application(current_user, current_venue, datetime, 1)
+
+    return make_response('Successfully apply!')
+
+
+@venue_api.route("/<vid>/delete/<datetime>", methods=["GET", "POST"])
+def delete(vid, datetime):
+    venue_manager = VenueManager()
+    user_manager = UserManager()
+    app_manager = ApplicationManager()
+
+    current_venue = venue_manager.get_venue_by_id(vid)
+
+    userid = session.get("userid")
+    current_user = user_manager.get_user_by_id(userid)
+
+    # delete application
+    app_manager.delete_application(user=current_user, venue=current_venue, datetime=datetime)
+
+    return make_response('Successfully delete!')
+
+
+@venue_api.route("/<vid>/get_applications", methods=["GET"])
+def get_applications(vid):
+    app_manager = ApplicationManager()
+
+    apps = app_manager.get_all_application()
+    app_data = [{"date": app.datetime, "user": app.user.username} for app in apps]
+
+    return jsonify(app_data)
--- venue/static/js/venue/index.js
+++ venue/static/js/venue/index.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
-    $('#datepicker').datepicker({
-        format: 'yyyy-mm',
-        autoclose: true,
-        todayHighlight: true,
-        language: 'zh-TW'
-    }).on('changeDate', function(e) {
-        generateCalendar(e.date);
-    });
-
-    function generateCalendar(date) {
+    function generateCalendar(date, applications) {
         var daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
         var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
         var calendar = $('#calendar');
@@ -20,23 +11,74 @@ $(document).ready(function() {
 
         for (var day = 1; day <= daysInMonth; day++) {
             var dayDate = new Date(date.getFullYear(), date.getMonth(), day);
-            var formattedDate = dayDate.toISOString().split('T')[0];
-            calendar.append('<div class="day" data-date="' + formattedDate + '" data-toggle="modal" data-target="#bookingModal">' + day + '</div>');
+            var formattedDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + day;
+
+            var application = applications.find(app => app.date === formattedDate);
+            console.log(application);
+            if (application) {
+                calendar.append('<div class="day reserved" data-date="' + formattedDate + '" data-toggle="modal" data-target="#bookingModal">' +
+                    day + '<br><small>' + application.user + '</small></div>');
+            }
+            else {
+                calendar.append('<div class="day" data-date="' + formattedDate + '" data-toggle="modal" data-target="#bookingModal">' + day + '</div>');
+            }
         }
     }
-    generateCalendar(new Date());
+
+    function loadApplicationsAndGenerateCalendar(date) {
+        $.ajax({
+            url: venueUrl,
+            type: 'GET',
+            success: function(applications) {
+                generateCalendar(date, applications);
+            },
+            error: function(error) {
+                console.log('Failed to load reservations: ', error);
+            }
+        });
+    }
+
+    $('#datepicker').datepicker({
+        format: 'yyyy-mm',
+        autoclose: true,
+        todayHighlight: true,
+        language: 'zh-TW',
+        minViewMode: 1
+    }).on('changeDate', function(e) {
+        loadApplicationsAndGenerateCalendar(e.date);
+    });
 
     $('#reserve').click(function() {
-        console.log(bookUrl);
         $.ajax({
             type: "POST",
-            url: bookUrl,
+            url: applyUrl,
             success: function(response) {
-                alert('success');
+                alert('success to apply!');
+                window.location.reload();
             },
             error: function(xhr, status, error) {
                 alert('failed: ' + error);
             }
         });
     });
+
+    $('#cancel').click(function() {
+        $.ajax({
+            type: "POST",
+            url: deleteUrl,
+            success: function(response) {
+                alert('success to delete!');
+                window.location.reload();
+            },
+            error: function(xhr, status, error) {
+                alert('failed: ' + error);
+            }
+        })
+    })
+
+    function formatDate(date) {
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        return year + '-' + ('0' + month).slice(-2);
+    }
 });
